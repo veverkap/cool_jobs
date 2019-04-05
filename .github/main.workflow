4d53b83d33a04100b1549850f64c8e4be8ea33d9
Patrick Veverka <patrick.veverka@gmail.com>
2019-04-04 21:38:36 -0400

    first commit

---
+++ .github/main.workflow
@@ -0,0 +1,15 @@
+workflow "Build JSON From Universe" {
+  resolves = ["../action/"]
+  on = "schedule(0 22 * * *)"
+}
+
+action "../action/" {
+  uses = "./action"
+  secrets = [
+    "TOKEN",
+    "MAILGUN_API_KEY",
+  ]
+  env = {
+    PAGES_BRANCH = "master"
+  }
+}
---
+++ action/entrypoint.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+if [[ -n "$TOKEN" ]]; then
+  GITHUB_TOKEN=$TOKEN
+fi
+
+if [[ -n "$PAGES_BRANCH" ]]; then
+  PAGES_BRANCH="master"
+fi
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+  echo "Set the GITHUB_TOKEN env variable."
+  exit 1
+fi
+
+if [[ -z "$GITHUB_REPOSITORY" ]]; then
+  echo "Set the GITHUB_REPOSITORY env variable."
+  exit 1
+fi
+
+main() {
+  cd "$GITHUB_WORKSPACE"
+  ruby runner.rb
+
+  echo "Starting deploy..."
+
+  echo "Changing git config"
+  git config --global url."https://".insteadOf git://
+  git config --global url."https://github.com/".insteadOf git@github.com:
+
+  git config user.name "GitHub Actions"
+  git config user.email "github-actions-bot@users.noreply.github.com"
+
+  remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  remote_branch=$PAGES_BRANCH
+
+  echo "Pushing artifacts to ${GITHUB_REPOSITORY}:$remote_branch"
+
+  git add .
+
+  if [ -n "$(git status --porcelain)" ]; then
+    echo "There are changes to the JSON files"
+
+    git commit -m "Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:$remote_branch"
+    git push --force "${remote_repo}" master:${remote_branch}
+  else
+    echo "There are no changes"
+  fi
+
+  echo "Deploy complete"
+}
+
+USERNAME=$(cat $GITHUB_EVENT_PATH | jq --raw-output '.commits[0].committer.username')
+
+if [ $USERNAME == "github-actions-bot" ]; then
+  echo "Don't need to do anything"
+else
+  echo "User is $USERNAME, continuing"
+  main "$@a"
+fi
